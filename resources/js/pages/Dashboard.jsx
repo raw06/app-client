521bfbdbe271a64cc3882fcd91df310da2dbab88
manhdat.tang <tangdatsdbg@gmail.com>
2024-01-18 11:29:04 +0700

    test

--- resources/js/pages/Dashboard.jsx
+++ resources/js/pages/Dashboard.jsx
@@ -43,7 +43,7 @@ export default function Dashboard() {
       if (!data.success) {
         showToast({
           error: true,
-          message: data
+          message: data.message
         });
       } else {
         showToast({
